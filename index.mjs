888d27878f7d98bcb33c9371890e4d7e9decfea5
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2024-02-23 15:06:43 -0300

    styled y boortrap

--- index.mjs
+++ index.mjs
@@ -1,10 +1,13 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 3030;
 
+app.use(express.static(path.resolve(new URL(import.meta.url).pathname, '../view')));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!s");
+  res.sendFile(path.join(__dirname, "view", "index.html"));
 });
 
 app.listen(port, () => {
