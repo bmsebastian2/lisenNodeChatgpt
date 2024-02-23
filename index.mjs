8d98f67c6bb3f4c3978acef6027e75f1b8070136
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2024-02-23 16:57:53 -0300

    Funcion de escucha

--- index.mjs
+++ index.mjs
@@ -4,12 +4,23 @@ import path from "path";
 const app = express();
 const port = 3030;
 
-app.use(express.static(path.resolve(new URL(import.meta.url).pathname, '../view')));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "view", "index.html"));
+// Configurar el directorio de archivos estáticos
+app.use(
+  express.static(path.resolve(new URL(import.meta.url).pathname, "../public"))
+);
+// Ruta para servir el archivo CSS
+app.get("/styles.css", (req, res) => {
+  res.sendFile(path.resolve("public", "styles.css"));
 });
-
+// Ruta para servir el archivo CSS
+app.get("/app.js", (req, res) => {
+  res.sendFile(path.resolve("public", "app.js"));
+});
+// Ruta para servir el archivo HTML
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("public", "index.html"));
+});
+// res.sendFile(path.join(__dirname, "public", "index.html"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
---
+++ public/app.js
@@ -0,0 +1,70 @@
+const btnGrabar = document.getElementById("grabar");
+const btnStop = document.getElementById("stop");
+const displayLoading = document.getElementById("displayLoading");
+
+// let cuadroTexto = document.getElementById("texto");
+// cuadroTexto.contains = "result[0].transcript";
+let cuadroTexto = document.getElementById("texto");
+
+let tiempoLimite = 5; // Tiempo límite en segundos
+let intervalo;
+
+btnStop.onclick = Stop;
+btnGrabar.onclick = Grabar;
+
+const recognition = new webkitSpeechRecognition();
+recognition.lang = "es-ES";
+recognition.continuous = true;
+
+function Grabar() {
+  hiden();
+  recognition.start();
+  recognition.onresult = (event) => {
+    iniciarContador();
+    for (const result of event.results) {
+      cuadroTexto.textContent = result[0].transcript;
+    }
+  };
+  recognition.onend = function () {
+    console.log("Reconocimiento de voz finalizado");
+  };
+  recognition.onnomatch = function (event) {
+    console.log("No se detectó ninguna coincidencia de voz");
+  };
+}
+
+function Stop() {
+  recognition.stop();
+  hiden();
+}
+
+function hiden() {
+  displayLoading.classList.toggle("hiden");
+  btnStop.classList.toggle("hiden");
+  btnGrabar.classList.toggle("hiden");
+}
+
+function detenerContador() {
+  clearInterval(intervalo);
+  console.log("Contador detenido");
+  // Llamar a tu función cuando se alcance el límite
+  // por ejemplo: ejecutarAlAlcanzarLimite();
+}
+function iniciarContador() {
+  let segundosRestantes = tiempoLimite;
+
+  intervalo = setInterval(function () {
+    segundosRestantes--;
+
+    if (segundosRestantes >= 0) {
+      actualizarContador(segundosRestantes);
+    } else {
+      Stop();
+      detenerContador();
+    }
+  }, 1000);
+}
+
+function actualizarContador(segundosRestantes) {
+  document.getElementById("tiempoRestante").innerText = segundosRestantes;
+}
