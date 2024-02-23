928c0b10b443e948e31f1468454c19f6bb710eed
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2024-02-23 18:32:22 -0300

    Change audio y button loading

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 const btnGrabar = document.getElementById("grabar");
 const btnStop = document.getElementById("stop");
 const displayLoading = document.getElementById("displayLoading");
-
+// const spinner = document.getElementById("spinner");
 // let cuadroTexto = document.getElementById("texto");
 // cuadroTexto.contains = "result[0].transcript";
 let cuadroTexto = document.getElementById("texto");
 
-let tiempoLimite = 5; // Tiempo límite en segundos
+let tiempoLimite = 0.5; // Tiempo límite en segundos
 let intervalo;
 
 btnStop.onclick = Stop;
@@ -17,14 +17,17 @@ recognition.lang = "es-ES";
 recognition.continuous = true;
 
 function Grabar() {
+  cuadroTexto.classList.remove("hiden");
   hiden();
   recognition.start();
   recognition.onresult = (event) => {
     iniciarContador();
+
     for (const result of event.results) {
       cuadroTexto.textContent = result[0].transcript;
     }
   };
+
   recognition.onend = function () {
     console.log("Reconocimiento de voz finalizado");
   };
@@ -34,22 +37,25 @@ function Grabar() {
 }
 
 function Stop() {
+  hiden();
+  console.log("stop");
+
   recognition.stop();
   detenerContador();
-  hiden();
 }
 
 function hiden() {
+  //   spinner.classList.toggle("hiden");
+
   displayLoading.classList.toggle("hiden");
   btnStop.classList.toggle("hiden");
   btnGrabar.classList.toggle("hiden");
 }
 
 function detenerContador() {
-  clearInterval(intervalo);
-  hiden();
-  console.log("Contador detenido");
   recognition.stop();
+  clearInterval(intervalo);
+  console.log("Contador detenido");
   // Llamar a tu función cuando se alcance el límite
   // por ejemplo: ejecutarAlAlcanzarLimite();
 }
@@ -62,11 +68,12 @@ function iniciarContador() {
     if (segundosRestantes >= 0) {
       actualizarContador(segundosRestantes);
     } else {
+      hiden();
       detenerContador();
     }
   }, 1000);
 }
 
 function actualizarContador(segundosRestantes) {
-  document.getElementById("tiempoRestante").innerText = segundosRestantes;
+  //   document.getElementById("tiempoRestante").innerText = segundosRestantes;
 }
